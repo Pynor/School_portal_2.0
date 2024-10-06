d8812fac86876e250aea4f689ff4a513444ae6cc
pynor <maksimowalex@mail.ru>
2024-10-06 13:31:09 +0300

    continuing work(Added tasks and answers): Improved models.

--- backend/task_app/admin.py
+++ backend/task_app/admin.py
@@ -1,2 +1,8 @@
+from .models import *
 from django.contrib import admin
 
+
+admin.site.register(Task)
+admin.site.register(Answer)
+admin.site.register(ListTasks)
+admin.site.register(ListAnswer)
--- backend/task_app/models.py
+++ backend/task_app/models.py
@@ -11,11 +11,14 @@ class Task(models.Model):
         ("Photo", "Take a picture of the answer")
     )
 
-    list_tasks = models.ForeignKey("ListTasks", verbose_name="List tasks", on_delete=models.CASCADE)
-    description = models.TextField(verbose_name="Description", max_length=300, null=True)
-    answer_to_the_task = models.CharField(verbose_name="Answer to the task", max_length=100)
-    additional_condition = models.CharField(verbose_name="Additional condition", choices=CONDITION_CHOICES, null=True, max_length=255)
     sequence_number = models.IntegerField(verbose_name="Sequence number")
+    answer_to_the_task = models.CharField(verbose_name="Answer to task", max_length=100)
+    title = models.CharField(verbose_name="Title to task", max_length=30, unique=True)
+    description = models.TextField(verbose_name="Description", max_length=300, null=True)
+    time_to_task = models.DurationField(verbose_name="Time to task", blank=True, null=True)
+    list_tasks = models.ForeignKey("ListTasks", verbose_name="List tasks", on_delete=models.CASCADE)
+    additional_condition = models.CharField(verbose_name="Additional condition", choices=CONDITION_CHOICES,
+                                            max_length=255, null=True)
 
     docx = models.FileField(upload_to="tasks_media/docx/",
                             validators=[FileExtensionValidator(allowed_extensions=["docx"])],
@@ -24,22 +27,31 @@ class Task(models.Model):
                              validators=[FileExtensionValidator(allowed_extensions=["mp4", "MPG", "mkv", "mov"])],
                              verbose_name="Video file", null=True)
 
+    def __str__(self):
+        return f"Task({self.sequence_number}): {self.title}"
+
 
 class ListTasks(models.Model):
-    event_date = models.TimeField(null=True)
-    task_for = models.ForeignKey(to=SchoolClass, verbose_name="Task for", on_delete=models.CASCADE)
     count_task = models.IntegerField(verbose_name="Count task")
+    title = models.CharField(max_length=255, unique=True, verbose_name="Title list tasks")
+    task_for = models.ForeignKey(to=SchoolClass, verbose_name="Task for", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Task list for: {self.task_for.title}"
 
 
 class Answer(models.Model):
+    answer = models.CharField(verbose_name="Answer", max_length=100, null=True)
     task = models.ForeignKey("Task", verbose_name="Task", on_delete=models.CASCADE)
     list_answer = models.ForeignKey("ListAnswer", verbose_name="List answer", on_delete=models.CASCADE)
-    answer = models.CharField(verbose_name="Answer", max_length=100, null=True)
-    photo_to_the_answer = models.ImageField(upload_to="tasks_media/images/", verbose_name="Photo to the answer", null=True)
+    photo_to_the_answer = models.ImageField(upload_to="tasks_media/images/", verbose_name="Photo to answer", null=True)
 
     def __str__(self):
-        return f"Answer:{self.answer}"
+        return f"Answer:{self.answer} to Task:{self.task.title}"
 
 
 class ListAnswer(models.Model):
     student = models.ForeignKey(to=Student, verbose_name="Student", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"List of student's answer: {self.student}"
