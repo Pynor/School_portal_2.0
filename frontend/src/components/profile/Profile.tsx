81cb6470732232dacd6126fff41b4b3f0653077b
pynor <maksimowalex@mail.ru>
2024-10-15 20:23:36 +0300

    improved: Frontend for Profile.

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom";
-import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './CSS/profile.css';
 
 const Profile = () => {
     return (
         <div className="form-container">
-            <Link to="/add-tasks" className="btn-primary">Создать тест</Link>
             <Link to="/register-student" className="btn-primary">Зарегистрировать учеников</Link>
+            <Link to="/add-tasks" className="btn-primary">Создать тест</Link>
         </div>
-        
     );
 };
 
