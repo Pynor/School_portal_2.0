282693a8ea264838e2a46fab5556967a02eed534
pynor <maksimowalex@mail.ru>
2024-10-19 21:05:09 +0300

    continuing work(Added tasks and answers): Added logic to add answers(additional).

--- backend/task_app/admin.py
+++ backend/task_app/admin.py
@@ -1,8 +1,8 @@
-from .models import *
 from django.contrib import admin
+from .models import *
 
 
-admin.site.register(Task)
-admin.site.register(Answer)
-admin.site.register(TaskList)
 admin.site.register(AnswerList)
+admin.site.register(TaskList)
+admin.site.register(Answer)
+admin.site.register(Task)
