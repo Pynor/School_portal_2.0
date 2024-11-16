57da79a4d687080187faf46e76cc3e72789c3fdc
pynor <maksimowalex@mail.ru>
2024-11-16 14:30:12 +0300

    continuing work(Added tasks and answers): Big improved and optimized task_app/services.

--- backend/task_app/services.py
+++ backend/task_app/services.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
@@ -8,23 +9,24 @@ from .models import Task, TaskList, Answer, AnswerList
 class TaskListAPIService:
     @staticmethod
     def create_task_list(validated_data: dict) -> TaskList:
-        task_for = SchoolClass.objects.get(title=validated_data.pop("task_for"))
+        task_for_title = validated_data.pop("task_for")
         tasks_data = validated_data.pop("tasks")
+        title = validated_data.pop("title")
 
-        task_list = TaskList.objects.create(task_for=task_for,
-                                            count_task=len(tasks_data),
-                                            title=validated_data.pop("title"))
+        task_for = SchoolClass.objects.get(title=task_for_title)
 
-        for task_data in tasks_data:
-            Task.objects.create(task_list=task_list, **task_data)
+        task_list = TaskList.objects.create(count_task=len(tasks_data),
+                                            task_for=task_for,
+                                            title=title)
+
+        tasks_to_create = [Task(task_list=task_list, **task_data) for task_data in tasks_data]
+        Task.objects.bulk_create(tasks_to_create)
 
         return task_list
 
     @staticmethod
     def get_task_list(kwargs) -> list[TaskList]:
-        task_for_id = SchoolClass.objects.filter(title=kwargs["school_class"]).values_list("id", flat=True).first()
-        task_list = TaskList.objects.filter(task_for=task_for_id)
-
+        task_list = TaskList.objects.select_related('task_for').filter(task_for__title=kwargs["school_class"])
         return task_list
 
 
@@ -40,36 +42,46 @@ class AnswerListAPIService:
 
         answer_list = AnswerList.objects.create(task_list=task_list, user=user)
 
-        for answer_data in answers_data:
-            Answer.objects.create(answer_list=answer_list, **answer_data)
+        answers_to_create = [Answer(answer_list=answer_list, **answer_data) for answer_data in answers_data]
+        Answer.objects.bulk_create(answers_to_create)
 
         return answer_list
 
     @staticmethod
     def get_students_answers_and_tasks_by_task_list(kwargs: dict, student_serializer,
                                                     answer_serializer, task_serializer) -> Response:
+
         task_list_id = kwargs["task_list_id"]
-        school_class = SchoolClass.objects.get(title=kwargs["school_class"])
-        students = Student.objects.filter(school_class=school_class).select_related("user")
+        school_class = SchoolClass.objects.prefetch_related(
+            Prefetch('student_set', queryset=Student.objects.select_related('user'))
+        ).get(title=kwargs["school_class"])
+
+        students = school_class.student_set.all()
+
+        answers = Answer.objects.filter(
+            answer_list__task_list=task_list_id,
+            answer_list__user__in=students.values_list('user', flat=True)
+        ).select_related("task")
+
+        answers_dict = {}
+        for answer in answers:
+            user_id = answer.answer_list.user.id
+            if user_id not in answers_dict:
+                answers_dict[user_id] = []
+            answers_dict[user_id].append(answer)
 
         answers_and_students = []
 
         for student in students:
-            answers = Answer.objects.filter(
-                answer_list__task_list=task_list_id,
-                answer_list__user=student.user
-            ).select_related("task")
-
-            serialized_answers = answer_serializer(answers, many=True)
-
-            tasks_and_answers = []
-
-            for answer, serialized_answer in zip(answers, serialized_answers.data):
-                task_data = task_serializer(answer.task).data if answer.task else None
-                tasks_and_answers.append({
-                    "answer": serialized_answer,
-                    "task": task_data
-                })
+            student_answers = answers_dict.get(student.user.id, [])
+            serialized_answers = answer_serializer(student_answers, many=True).data
+            tasks_and_answers = [
+                {
+                    "answer": serialized_answers[i],
+                    "task": task_serializer(student_answers[i].task).data if student_answers[i].task else None
+                }
+                for i in range(len(student_answers))
+            ]
 
             answers_and_students.append({
                 "student": student_serializer(student).data,
@@ -77,4 +89,3 @@ class AnswerListAPIService:
             })
 
         return Response(answers_and_students)
-
