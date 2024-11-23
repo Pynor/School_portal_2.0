c30c342272f2a32cff0b4e4f0e63d23b9462a41b
pynor <maksimowalex@mail.ru>
2024-11-23 18:06:21 +0300

    continuing work(Added test for tasks and answers) Improved test_views.

--- backend/tests/task_tests/test_views.py
+++ backend/tests/task_tests/test_views.py
@@ -3,15 +3,17 @@ from rest_framework import status
 
 
 from task_app.models import TaskList, Task, AnswerList, Answer
-from user_app.models import SchoolClass, User
+
+from user_app.models import SchoolClass, Student, User
 
 
-class AnswerListCreateAPITest(APITestCase):
+class AnswerListAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.task_for = SchoolClass.objects.create(title="9Г", slug="9Г")
         self.user = User.objects.create_user(username="Student", password="password")
+        self.student = Student.objects.create(school_class=self.task_for, user=self.user, authorized=True)
 
-        self.task_list = TaskList.objects.create(count_task=2, title="Test task", task_for=self.task_for)
+        self.task_list = TaskList.objects.create(task_for=self.task_for, count_task=2, title="Test task")
 
         self.task_1 = Task.objects.create(description="task_description_1", answer_to_the_task="answer",
                                           task_list=self.task_list, sequence_number=1, title="task_1")
@@ -38,9 +40,10 @@ class AnswerListCreateAPITest(APITestCase):
 
             ]
         }
+
         response = client.post("/task_app/api/v1/api-answer-list-create/", data, format="json")
 
-        self.assertEqual(AnswerList.objects.get().user.username, "Student")
+        self.assertEqual(AnswerList.objects.get().user.username, self.user.username)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(AnswerList.objects.count(), 1)
         self.assertEqual(Answer.objects.count(), 2)
@@ -55,3 +58,86 @@ class AnswerListCreateAPITest(APITestCase):
         response = client.get(f"/task_app/api/v1/api-answer-list-get/{self.task_for.title}/{self.task_list.id}")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["tasks_and_answers"][0]["answer"]["answer"], self.answer_1.answer)
+        self.assertEqual(response.data[0]["tasks_and_answers"][1]["answer"]["answer"], self.answer_2.answer)
+        self.assertEqual(response.data[0]["tasks_and_answers"][0]["task"]["title"], self.task_1.title)
+        self.assertEqual(response.data[0]["tasks_and_answers"][1]["task"]["title"], self.task_2.title)
+        self.assertEqual(response.data[0]["tasks_and_answers"][0]["answer"]["task"], self.task_1.id)
+        self.assertEqual(response.data[0]["tasks_and_answers"][1]["answer"]["task"], self.task_2.id)
+        self.assertEqual(response.data[0]["student"]["school_class"], self.task_for.title)
+        self.assertTrue(response.data[0]["student"]["authorized"])
+
+
+class TaskListAPITestCase(APITestCase):
+    def setUp(self) -> None:
+        self.task_for = SchoolClass.objects.create(title="9Г", slug="9Г")
+        self.user = User.objects.create_user(username="Student", password="password")
+        self.student = Student.objects.create(school_class=self.task_for, user=self.user, authorized=True)
+
+    def test_create_task_list(self) -> None:
+        client = APIClient()
+        data: dict[str, list] = {
+            "title": "Test task list",
+            "count_task": 2,
+            "task_for": "9Г",
+            "tasks": [
+                {
+                  "sequence_number": 1,
+                  "answer_to_the_task": "2",
+                  "title": "Task 1",
+                  "description": "1+1=",
+                  "additional_condition": "None"
+                },
+                {
+                  "sequence_number": 2,
+                  "answer_to_the_task": "4",
+                  "title": "Task 2",
+                  "description": "2+2=",
+                  "additional_condition": "Photo",
+                  "time_to_task": "PT1H30M45S"
+                }
+            ]
+        }
+
+        response = client.post("/task_app/api/v1/api-task-list-create/", data, format="json")
+
+        self.assertEqual(TaskList.objects.get().title, "Test task list")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TaskList.objects.count(), 1)
+        self.assertEqual(Task.objects.count(), 2)
+
+    def test_get_answers_list(self) -> None:
+        self.task_list_1 = TaskList.objects.create(task_for=self.task_for, title="Test task list 1", count_task=1)
+
+        self.task_1_1 = Task.objects.create(additional_condition="None", answer_to_the_task="answer_1_1",
+                                            description="task_description_1", time_to_task="PT1H30M45S",
+                                            task_list=self.task_list_1, sequence_number=1,
+                                            title="task_1_1")
+
+        self.task_list_2 = TaskList.objects.create(task_for=self.task_for, title="Test task list 2", count_task=2)
+
+        self.task_2_1 = Task.objects.create(description="task_description_1", answer_to_the_task="answer_2_1",
+                                            additional_condition="Photo", task_list=self.task_list_2,
+                                            sequence_number=1, title="task_2_1")
+
+        self.task_2_2 = Task.objects.create(description="task_description_2", answer_to_the_task="answer_2_2",
+                                            additional_condition="None", task_list=self.task_list_2,
+                                            sequence_number=2, title="task_2_2")
+
+        client = APIClient()
+        response = client.get(f"/task_app/api/v1/api-task-list-get/{self.task_for}")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["task_list"][0]["tasks"][0]["title"], self.task_1_1.title)
+        self.assertEqual(response.data["task_list"][1]["tasks"][0]["title"], self.task_2_1.title)
+        self.assertEqual(response.data["task_list"][1]["tasks"][1]["title"], self.task_2_2.title)
+        self.assertEqual(response.data["task_list"][0]["tasks"][0]["time_to_task"], "01:30:45")
+        self.assertEqual(response.data["task_list"][1]["tasks"][0]["time_to_task"], None)
+
+        self.assertEqual(response.data["task_list"][1]["tasks"][0]["additional_condition"],
+                         self.task_2_1.additional_condition)
+        self.assertEqual(response.data["task_list"][1]["tasks"][1]["additional_condition"],
+                         self.task_2_2.additional_condition)
+
+        self.assertEqual(response.data["task_list"][0]["task_for"], self.task_for.title)
+        self.assertEqual(response.data["task_list"][1]["task_for"], self.task_for.title)
--- backend/tests/user_tests/test_views.py
+++ backend/tests/user_tests/test_views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from user_app.models import User, Teacher, Student, TeacherSecretKey, SchoolClass
 
 
-class TeacherRegisterAPITest(APITestCase):
+class TeacherRegisterAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.secret_key = TeacherSecretKey.objects.create(key="123")
 
@@ -20,13 +20,15 @@ class TeacherRegisterAPITest(APITestCase):
             },
             "secret_key": "123"
         }
+
         response = client.post("/user_app/api/v1/api-teacher-register/", data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Teacher.objects.count(), 1)
         self.assertEqual(User.objects.get().username, "Teacher")
 
 
-class StudentsRegisterListAPITest(APITestCase):
+class StudentsRegisterListAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.school_class = SchoolClass.objects.create(title="9Г", slug="9Г")
 
@@ -63,7 +65,7 @@ class StudentsRegisterListAPITest(APITestCase):
             User.objects.filter(first_name="Mike", last_name="Johnson", username="MikeJohnson9Г").count(), 1)
 
 
-class TeacherLoginAPITest(APITestCase):
+class TeacherLoginAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.secret_key = TeacherSecretKey.objects.create(key="123")
         self.user = User.objects.create_user(username="Teacher", password="password")
@@ -75,12 +77,14 @@ class TeacherLoginAPITest(APITestCase):
             "username": "Teacher",
             "password": "password",
         }
+
         response = client.post("/user_app/api/v1/api-teacher-login/", data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("jwt_token", response.data)
 
 
-class StudentLoginAPITest(APITestCase):
+class StudentLoginAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.school_class = SchoolClass.objects.create(title="9Г", slug="9Г")
         self.user = User.objects.create_user(username="JohnDoe9Г", password="JD", first_name="John", last_name="Doe")
@@ -94,6 +98,7 @@ class StudentLoginAPITest(APITestCase):
             "password": "JD",
             "school_class": "9Г"
         }
+
         response = client.post("/user_app/api/v1/api-student-login/", data, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -101,7 +106,7 @@ class StudentLoginAPITest(APITestCase):
         self.assertIn("jwt_token", response.data)
 
 
-class UserAPITest(APITestCase):
+class UserAPITestCase(APITestCase):
     def setUp(self) -> None:
         self.client = APIClient()
 
@@ -125,6 +130,7 @@ class UserAPITest(APITestCase):
                                                        }).data["jwt_token"]
 
         self.client.credentials(HTTP_AUTHORIZATION="jwt_token " + self.student_jwt_token)
+
         response = self.client.get("/user_app/api/v1/api-user-get/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -136,6 +142,7 @@ class UserAPITest(APITestCase):
                                                        format="json").data["jwt_token"]
 
         self.client.credentials(HTTP_AUTHORIZATION="jwt_token " + self.teacher_jwt_token)
+
         response = self.client.get("/user_app/api/v1/api-user-get/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
