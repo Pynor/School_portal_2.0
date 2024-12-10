fb6281d0d9557d66a157063fbfb87243b8e213bd
pynor <maksimowalex@mail.ru>
2024-12-10 21:46:14 +0300

    continuing work(Added tasks and answers): Start work on displaying answers in CheckAnswers.

--- frontend/src/components/tasks-and-answers/CheckAnswers.tsx
+++ frontend/src/components/tasks-and-answers/CheckAnswers.tsx
@@ -5,14 +5,18 @@ import { StudentAndAnswerForCheckAnswers, UserData } from '../../types';
 import { BASE_URL } from '../../constants';
 
 import './CSS/add-task.css';
+import Modal from './ModalWindows';
 
 
 const CheckAnswers: React.FC<{ userData: UserData }> = ({ userData }) => {
 
     const [data, setData] = useState<StudentAndAnswerForCheckAnswers[]>();
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const { schoolClass, taskListId } = useParams();
     const hasFetchedRef = useRef(false);
+
+    console.log(data)
 
     useEffect(() => {
         const getAnswerLists = async () => {
@@ -47,7 +51,7 @@ const CheckAnswers: React.FC<{ userData: UserData }> = ({ userData }) => {
 
     return (
         <div className="form-container">
-            <Link to="/check-answers-hub" className="btn-primary" style={{alignSelf: "flex-start"}}>Вернутся</Link>
+            <Link to="/check-answers-hub" className="btn-primary" style={{ alignSelf: "flex-start" }}>Вернутся</Link>
             {errorMessage && <h2 className="error-message">{errorMessage}</h2>}
             {userData.is_staff ? (
                 <div>
@@ -63,8 +67,27 @@ const CheckAnswers: React.FC<{ userData: UserData }> = ({ userData }) => {
                                     ) : (
                                         student_answer_and_task.tasks_and_answers.map((answer_and_task, index) => (
                                             <div key={index} className="form-container">
-                                                <h2>Задача: {answer_and_task.task.answer_to_the_task}</h2>
-                                                <h3>Ответ Ученика: {answer_and_task.answer.answer}</h3>
+                                                <h2>Задача: {answer_and_task.task.title}</h2> 
+                                                {answer_and_task.answer.answer ? (
+                                                    <h3>Ответ: {answer_and_task.answer.answer}</h3>
+                                                ) : (
+                                                    <h3 className="normal-message">Ответ: отсутствует.</h3>
+                                                )}
+                                                {answer_and_task.answer.photo_to_the_answer && (
+                                                    <div>
+                                                        <img
+                                                            alt="Загруженное"
+                                                            onClick={() => setIsModalOpen(true)}
+                                                            src={`${BASE_URL}${answer_and_task.answer.photo_to_the_answer}`}
+                                                            style={{ width: '290px', height: '200px', objectFit: 'cover' }}
+                                                        />
+                                                        <Modal
+                                                            isOpen={isModalOpen}
+                                                            onClose={() => setIsModalOpen(false)}
+                                                            imageSrc={`${BASE_URL}${answer_and_task.answer.photo_to_the_answer}`}
+                                                        />
+                                                    </div>
+                                                )}
                                             </div>
                                         ))
                                     )
