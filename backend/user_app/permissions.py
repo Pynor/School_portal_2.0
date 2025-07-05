347d19ff547312e2a939f43997f0f17be3706042
Pynor <pynor31@mail.ru>
2025-07-05 23:06:14 +0300

    Fixed: The problem of verifying registration rights.

--- backend/user_app/permissions.py
+++ backend/user_app/permissions.py
@@ -2,8 +2,14 @@ from rest_framework import permissions
 
 class IsTeacher(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_staff
+        return (
+                (request.user and request.user.is_staff) or
+                (request.headers.get('X-Is-Staff') == 'true')
+        )
 
 class IsStudent(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and not request.user.is_staff
+        return (
+                (request.user and request.user.is_staff) or
+                (request.headers.get('X-Is-Staff') == 'false')
+        )
